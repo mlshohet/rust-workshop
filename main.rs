94f5b2573d179cb66ab80ad424f376a4081515d7
SHOHET, MICHAEL <michaelshohet@northwesternmutual.com>
2022-01-17 14:59:05 -0500

    first commit

---
+++ main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // mut is a mutable variable
+    let mut message = "Hello World!";
+    message = "Hi there!";
+    println!("{}", message);
+
+    let mut age = 10;
+    age = 30;
+    println!("Age: {}", age);
+}
